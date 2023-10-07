8bd9723b61196abc08061c87ec1ca521568e7a12
xavajk <95323308+xavajk@users.noreply.github.com>
2023-10-07 03:12:30 -0700

    Time: 453 ms (6.69%), Space: 16.3 MB (82.63%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,42 +1,26 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        # res, perm = [], []
-        # def backtrack(i, seen):
-        #     # if we have a complete permuation
-        #     if len(perm) == len(nums):
-        #         res.append(perm.copy())
-        #         return
-        #     # out of bounds
-        #     if i == len(nums):
-        #         return
-        #     # for each number not seen in nums
-        #     # either take or dont take
-        #     for n in nums:
-        #         if n in seen: 
-        #             continue
-        #         # take
-        #         perm.append(n)
-        #         seen.add(n)
-        #         backtrack(i + 1, seen)
-        #         # dont take
-        #         perm.pop()
-        #         seen.remove(n)
-        #         backtrack(i + 1, seen)
-        # backtrack(0, set())
-        # return res
-
-        res = []
-
-        # base case
-        if len(nums) == 1:
-            return [nums[:]]  # nums[:] is a deep copy
-
-        for i in range(len(nums)):
-            n = nums.pop(0)
-            perms = self.permute(nums)
-
-            for perm in perms:
+        res, perm = [], []
+        def backtrack(i, seen):
+            # if we have a complete permuation
+            if len(perm) == len(nums):
+                res.append(perm.copy())
+                return
+            # out of bounds
+            if i == len(nums):
+                return
+            # for each number not seen in nums
+            # either take or dont take
+            for n in nums:
+                if n in seen: 
+                    continue
+                # take
                 perm.append(n)
-            res.extend(perms)
-            nums.append(n)
+                seen.add(n)
+                backtrack(i + 1, seen)
+                # dont take
+                perm.pop()
+                seen.remove(n)
+                backtrack(i + 1, seen)
+        backtrack(0, set())
         return res
