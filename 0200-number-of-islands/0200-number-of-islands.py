ceb5220a5893cfbbd5d1917e962e6e0037f8be36
xavajk <95323308+xavajk@users.noreply.github.com>
2023-10-07 14:57:06 -0700

    Time: 370 ms (9.85%), Space: 24 MB (41.08%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,23 +2,39 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         seen = set()
         m, n = len(grid), len(grid[0])
-        def dfs(i, j):
-            # base case: out of bounds or at '0'
-            if i not in range(m) or j not in range(n):
-                return
-            if grid[i][j] == '0' or (i, j) in seen:
-                return 
+        
+        # DFS Implementation
+        # def dfs(i, j):
+        #     # base case: out of bounds or at '0'
+        #     if i not in range(m) or j not in range(n):
+        #         return
+        #     if grid[i][j] == '0' or (i, j) in seen:
+        #         return 
+        #     seen.add((i, j))
+        #     # recurse in all directions
+        #     dfs(i + 1, j)
+        #     dfs(i - 1, j)
+        #     dfs(i, j + 1)
+        #     dfs(i, j - 1)
+
+        # BFS Implementation (more suited to finding islands)
+        def bfs(i, j):
+            queue = deque()
             seen.add((i, j))
-            # recurse in all directions
-            dfs(i + 1, j)
-            dfs(i - 1, j)
-            dfs(i, j + 1)
-            dfs(i, j - 1)
-            
+            queue.append((i, j))
+            while queue:
+                r, c = queue.popleft()
+                dirs = [ (r + dr, c + dc) for dr, dc in [[1, 0], [-1, 0], [0, 1], [0, -1]] ]
+                for i, j in dirs:
+                    if i in range(m) and j in range(n) and grid[i][j] == '1' and (i, j) not in seen:
+                        queue.append((i, j))
+                        seen.add((i, j))
+
         islands = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == '1' and (i, j) not in seen:
+                    bfs(i, j)
                     islands += 1
-                    dfs(i, j)
+                    # dfs(i, j)
         return islands
